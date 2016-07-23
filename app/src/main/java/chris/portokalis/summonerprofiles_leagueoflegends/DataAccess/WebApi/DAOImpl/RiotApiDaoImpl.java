c4c5319c381493d886fe528c7a2a0ec5978d9701
Chris <cportokalis@gmail.com>
2016-07-23 18:41:09 -0500

    Refactoring project structure

---
+++ app/src/main/java/chris/portokalis/summonerprofiles_leagueoflegends/DataAccess/WebApi/DAOImpl/RiotApiDaoImpl.java
@@ -0,0 +1,16 @@
+package chris.portokalis.summonerprofiles_leagueoflegends.DataAccess.WebApi.DAOImpl;
+
+import org.json.JSONObject;
+
+import chris.portokalis.summonerprofiles_leagueoflegends.DataAccess.WebApi.DAO.RiotApiDao;
+
+/**
+ * Created by Chris on 7/23/2016.
+ */
+public class RiotApiDaoImpl implements RiotApiDao
+{
+    @Override
+    public JSONObject getSummonerInfo(String summonerName, String region) {
+        return null;
+    }
+}
